ca088f24c97e78ebde357428b0b718e317e1205d
Mariusz <devrogm@gmail.com>
2024-02-14 18:15:33 +0100

    Improve get_next_game function

--- torg_api/games/views.py
+++ torg_api/games/views.py
@@ -18,12 +18,10 @@ class GameDetailsView(generics.RetrieveUpdateDestroyAPIView):
 
     def perform_update(self, serializer):
         game = self.get_object()
-        if serializer.validated_data['is_approved'] and not self.get_object().is_approved:
+        if serializer.validated_data['is_approved'] and not game.is_approved:
             winner_obj = get_winner(game.player_1, game.player_2, game.score_1, game.score_2)
-            if winner_obj:
-                next_game_round, next_game_num = get_next_game(game.game_round, game.game_num)
-                next_game = Game.objects.get(tournament=game.tournament, game_round=next_game_round,
-                                             game_num=next_game_num)
+            next_game = get_next_game(game.game_round, game.game_num, game.tournament)
+            if winner_obj and next_game:
                 if not next_game.player_1:
                     next_game.player_1 = winner_obj
                 elif not next_game.player_2 and next_game.player_1 != winner_obj:
