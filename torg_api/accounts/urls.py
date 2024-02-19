5af05db1f1fcbbf4d4999cd981b73cb32a9548a9
Mariusz <devrogm@gmail.com>
2024-02-19 21:38:31 +0100

    Add logout

--- torg_api/accounts/urls.py
+++ torg_api/accounts/urls.py
@@ -5,4 +5,5 @@ app_name = 'accounts'
 urlpatterns = [
     path('register/', views.UserRegisterView.as_view(), name='register'),
     path('login/', views.user_login, name='user_login'),
+    path('logout/', views.user_logout, name='user_logout'),
 ]
--- torg_api/accounts/views.py
+++ torg_api/accounts/views.py
@@ -2,11 +2,12 @@ from .serializers import UserSerializer, LoginSerializer
 from rest_framework import generics
 from rest_framework.authtoken.models import Token
 from django.contrib.auth import authenticate
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenticated
 
 
 class UserRegisterView(generics.CreateAPIView):
@@ -34,3 +35,15 @@ def user_login(request):
             return Response({'token': token.key}, status=status.HTTP_200_OK)
 
         return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def user_logout(request):
+    if request.method == 'POST':
+        try:
+            # Delete the user's token to logout
+            request.user.auth_token.delete()
+            return Response({'message': 'Successfully logged out.'}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
