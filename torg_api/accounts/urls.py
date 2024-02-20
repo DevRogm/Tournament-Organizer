61afa771be71fdb15455dcb74aa97e995412682f
Mariusz <devrogm@gmail.com>
2024-02-21 00:29:31 +0100

    Add signals to save username as email after save user

--- torg_api/accounts/urls.py
+++ torg_api/accounts/urls.py
@@ -4,8 +4,8 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('user/', views.get_user_profile, name='user_profile')
-    # path('register/', views.UserRegisterView.as_view(), name='register'),
+    path('user/', views.get_user_profile, name='user'),
+    path('register/', views.register_user, name='register'),
     # path('login/', views.user_login, name='user_login'),
     # path('logout/', views.user_logout, name='user_logout'),
 ]
--- torg_api/accounts/views.py
+++ torg_api/accounts/views.py
@@ -1,11 +1,33 @@
-from .serializers import UserSerializer, MyTokenObtainPairSerializer
+from .serializers import UserSerializer, MyTokenObtainPairSerializer, UserSerializerWithToken
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
 
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
+
+
+@api_view(['POST'])
+def register_user(request):
+    data = request.data
+
+    try:
+        user = User.objects.create(
+            first_name=data['name'],
+            username=data['email'],
+            email=data['email'],
+            password=make_password(data['password'])
+        )
+        serializer = UserSerializerWithToken(user, many=False)
+        return Response(serializer.data)
+    except:
+        message = {'detail':'User with this email already exists'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['GET'])
 def get_user_profile(request):
