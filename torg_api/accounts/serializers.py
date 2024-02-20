d0ab7ade3a1e742eff98afdad586132535f8bb31
Mariusz <devrogm@gmail.com>
2024-02-20 22:16:32 +0100

    Add refresh token

--- torg_api/accounts/serializers.py
+++ torg_api/accounts/serializers.py
@@ -1,14 +1,16 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         data = super().validate(attrs)
 
-        data["username"] = self.user.username
-        data["email"] = self.user.email
+        serializer = UserSerializerWithToken(self.user).data
+        for k, v in serializer.items():
+            data[k] = v
         return data
 
 
@@ -21,3 +23,15 @@ class UserSerializer(serializers.ModelSerializer):
 
     def get_is_admin(self, obj):
         return obj.is_staff
+
+
+class UserSerializerWithToken(UserSerializer):
+    token = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'is_admin', 'token']
+
+    def get_token(self, obj):
+        token = RefreshToken.for_user(obj)
+        return str(token.access_token)
--- torg_api/accounts/views.py
+++ torg_api/accounts/views.py
@@ -1,13 +1,6 @@
 from .serializers import UserSerializer, MyTokenObtainPairSerializer
-from rest_framework import generics
-from rest_framework.authtoken.models import Token
-from django.contrib.auth import authenticate
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
-from rest_framework import status
-from django.core.exceptions import ObjectDoesNotExist
-from django.contrib.auth.models import User
-from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 
