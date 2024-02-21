f4d0fed3550dcfdc3548adfb22e0b6555a7c2e92
Mariusz <devrogm@gmail.com>
2024-02-21 12:33:25 +0100

    Code refactor

--- torg_api/accounts/urls.py
+++ torg_api/accounts/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from . import views
+
 app_name = 'accounts'
 
 urlpatterns = [
     path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('user/', views.get_user_profile, name='user'),
     path('register/', views.register_user, name='register'),
-    # path('login/', views.user_login, name='user_login'),
-    # path('logout/', views.user_logout, name='user_logout'),
 ]
